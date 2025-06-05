739f51621f46895bd440b236aac8efdb7b5953a1
Isaq <isaque3356@icloud.com>
2025-06-05 20:23:01 -0300

    Adicionar arquivos que faltantes nas branch:
    SQL:
    calisto.sql
    
    Classes:
    Agencia.java
    Conta.java
    Endereco.java
    Investimento.java
    Transacao.java
    
    Enum:
    PerfilRisco.java
    Status.java

---
+++ src/main/java/CALISTO/model/persistence/Endereco/Endereco.java
@@ -0,0 +1,101 @@
+package CALISTO.model.persistence.Endereco;
+
+import CALISTO.model.persistence.Usuario.Usuario;
+
+public class Endereco {
+    private int idEndereco;
+//    private Usuario usuario;
+    private String cep;
+    private String local;
+    private int numeroCasa;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String complemento;
+
+    public Endereco(String cep, String local, String numeroCasa, String bairro, String cidade, String estado, String complemento) {
+        this.cep = cep;
+        this.local = local;
+        this.numeroCasa = Integer.parseInt(numeroCasa);
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.complemento = complemento;
+    }
+
+    public Endereco() {
+
+    }
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+//    public Usuario getUsuario() {
+//        return usuario;
+//    }
+//
+//    public void setUsuario(Usuario usuario) {
+//        this.usuario = usuario;
+//    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
---
+++ src/main/java/CALISTO/model/persistence/util/Status.java
@@ -0,0 +1,7 @@
+package CALISTO.model.persistence.util;
+
+public enum Status {
+    ATIVO,
+    ENCERRADA,
+    BLOQUEADA
+}
